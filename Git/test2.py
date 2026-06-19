b9ee1ddad4d710f4bc48c30ceb7411fa77e187ce
Mohammed Fiyas <mfiyass@gmail.com>
2026-06-19 16:27:02 +0530

    update

--- Git/test2.py
+++ Git/test2.py
@@ -1,2 +1,4 @@
 #this is the second file 
 print("Hello World!")
+print("This is a test file for Git.")
+
--- Git/testfile.py
+++ Git/testfile.py
@@ -2,3 +2,4 @@
 print("bye World!")
 print("This is a test file for Git.")
 print("Let's see if we can commit this file successfully.")
+print("This is the last line of the file.")
