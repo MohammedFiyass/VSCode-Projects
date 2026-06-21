44e9fbca931376889039ec58a2acf5fec5dd83b5
Mohammed Fiyas <mfiyass@gmail.com>
2026-06-21 22:41:50 +0530

    delete

--- Git/test2.py
+++
@@ -1,4 +0,0 @@
-#this is the second file 
-print("Hello World!")
-print("This is a test file for Git.")
-
--- Git/testfile.py
+++
@@ -1,7 +0,0 @@
-#This is the first file in the Github from VScode 
-print("bye World!")
-print("This is a test file for Git.")
-print("Let's see if we can commit this file successfully.")
-print("This is the last line of the file.")
-print("This is from the Branch update")
-
